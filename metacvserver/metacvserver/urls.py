dd89d1e97379d81c94c90055ba10e9273ac1b5f5
Mister Tree <mistertree@openmailbox.org>
2015-12-27 11:33:10 +0100

    The view are now more DRY and using class based views

--- metacvserver/metacvserver/urls.py
+++ metacvserver/metacvserver/urls.py
@@ -1,13 +1,15 @@
 # pylint: disable=bad-continuation
 
+from cv.views import TheMainTemplateView, HashtagAjaxView, FeatureAjaxView, switch_ajax
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'cv.views.mainpage', name='mainpage'),
-    url(r'^(?P<hashtag_id>[a-zA-Z]*)/?$', 'cv.views.hashtag', name='hashtag'),
+    url(r'^$', TheMainTemplateView.as_view(), name='mainpage'),
+    url(r'^(?P<hashtag_id>[a-zA-Z]*)/?$', switch_ajax(HashtagAjaxView), 
+        name='hashtag'),
     url(r'^(?P<hashtag_id>[a-zA-Z]*)/(?P<feature_id>[a-zA-Z]*)/?$',
-        'cv.views.feature', name='feature')
+        switch_ajax(FeatureAjaxView), name='feature')
 )
