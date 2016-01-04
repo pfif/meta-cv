d26186889613aa05dd028058dbb5a39707b6ea1d
Mister Tree <mistertree@openmailbox.org>
2016-01-04 15:18:21 +0000

    Splited URLs in several files

--- metacvserver/metacvserver/urls.py
+++ metacvserver/metacvserver/urls.py
@@ -1,17 +1,15 @@
 # pylint: disable=bad-continuation
 
-from cv.views import TheMainTemplateView, HashtagAjaxView, FeatureAjaxView, switch_ajax
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+
+from cv import urls
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TheMainTemplateView.as_view(), name='mainpage'),
-    url(r'^(?P<hashtag_id>[a-zA-Z]*)/?$', switch_ajax(HashtagAjaxView), 
-        name='hashtag'),
-    url(r'^(?P<hashtag_id>[a-zA-Z]*)/(?P<feature_id>[a-zA-Z]*)/?$',
-        switch_ajax(FeatureAjaxView), name='feature')
+    url(r'^', include(urls))
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
